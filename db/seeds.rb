931bebdddfbe19ca50cd6b210fe2f30aaf668f4e
beranemo <beranemo@gmail.com>
2017-12-15 20:32:14 +0800

    add seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@ Category.destroy_all
 category_list = [
   { name: "中式料理"},
   { name: "日本料理"},
-  { name: "義大利料理"}
+  { name: "義大利料理"},
+  { name: "墨西哥料理"},
+  { name: "素食料理"},
+  { name: "美式料理"},
+  { name: "複合式料理"}
   ]
 
 
