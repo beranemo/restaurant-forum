b48bb6e65c6dfd2373550dd8f8ccee4521376c82
beranemo <beranemo@gmail.com>
2017-12-17 01:37:14 +0800

    fix the bug to type the error code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
-  
-  @category = Category.find(params[:id])
-  
+    
+  before_action :authenticate_user!
+
 end
