9438dc6853cab2c75ff67babcd37303192d1cf22
beranemo <beranemo@gmail.com>
2017-12-17 03:19:41 +0800

    take a break

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   # validates_presence_of :name
-  
+    
   mount_uploader :image, PhotoUploader
   
   belongs_to :category, optional: true # 允許外鍵 nil
