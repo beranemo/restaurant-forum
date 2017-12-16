9daab386c777d52dd9854aa83286a8b2446ff23f
beranemo <beranemo@gmail.com>
2017-12-17 00:24:24 +0800

    move authenticate_use! to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  @category = Category.find(params[:id])
+  
   private
   
   def authenticate_admin
