e84c357d659bdc2fecd6b9e848509fe76857ac48
beranemo <beranemo@gmail.com>
2017-12-20 23:40:13 +0800

    modify if comment is blank

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,13 @@ class CommentsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
     @comment.user_id = current_user.id
-    @comment.save! # 與 @comment.save 作比較
-    redirect_to restaurant_path(@restaurant)
+    #@comment.save! # 與 @comment.save 作比較
+    if @comment.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # render template: 'restaurants/show'
+      redirect_to request.referrer, alert: "Comment不得為空白" 
+    end
   end
   
   def destroy
