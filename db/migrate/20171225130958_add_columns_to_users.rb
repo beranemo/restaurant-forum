fc31e86c02a68cb59718d74122027459a542da46
beranemo <beranemo@gmail.com>
2017-12-25 21:12:45 +0800

    rails g migration add_columns_to_users

---
+++ db/migrate/20171225130958_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :intro, :text
+    add_column :users, :avatar, :string
+  end
+end
