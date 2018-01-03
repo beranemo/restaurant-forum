f25d1977e5688a76e22a0cf6415d16125504e2cd
beranemo <beranemo@gmail.com>
2018-01-04 01:54:14 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,15 @@
 class FollowshipsController < ApplicationController
+  
+  def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    
+    if @followship.save
+      flash[:notice] = "追蹤成功"
+      redirect_back(fallback_location: root_path)  # 導回上一頁
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)  # 導回上一頁
+    end
+  end
+  
 end
