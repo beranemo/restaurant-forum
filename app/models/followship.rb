69609aff96f5281eeea1fd9cbef326ae62eecd5f
beranemo <beranemo@gmail.com>
2018-01-04 01:53:29 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -12,4 +12,8 @@
 class Followship < ApplicationRecord
   belongs_to :user
   belongs_to :following, class_name: "User"
+  
+  validates :following_id, uniqueness: { scope: :user_id }  
+  # validates :user_id, uniqueness: { scope: :following_id }
+  
 end
